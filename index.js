fea980f702b3e60af477daff1453c5a6cd74357f
David Pink <dvdpink@gmail.com>
2019-11-04 17:35:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,4 +30,8 @@ function prependKitten(name) {
 
 function removeLastKitten(){
   return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(-2)
 }// Add your functions and code here
